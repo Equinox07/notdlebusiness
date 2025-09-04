9bcafd7604b5cbf407d4ecabae3b1f2f82e6d159
Hope Avalon <hopegag@gmail.com>
2025-09-04 17:34:59 +0000

    updated measurement screen

--- lib/pages/screens/add_customer_screen.dart
+++ lib/pages/screens/add_customer_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notdle/db/database_helper.dart';
 import 'package:notdle/models/customer.dart';
+import 'package:notdle/pages/screens/add_measurement_screen.dart';
 import 'package:notdle/pages/screens/measurements_screen.dart';
 import 'package:notdle/pages/screens/orders_screen.dart';
 
@@ -229,7 +230,7 @@ class _AddCustomerScreenState extends State<AddCustomerScreen> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (_) => MeasurementsScreen(customer: saved),
+                        builder: (_) => AddMeasurementScreen(customer: saved),
                       ),
                     );
                   }

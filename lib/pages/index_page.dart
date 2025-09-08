6603e645d30010b6f6fb7860891e7b1859d42470
Hope Avalon <hopegag@gmail.com>
2025-09-08 16:50:32 +0000

    revove dbg banner

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -22,6 +22,7 @@ class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: ''),
       home: _StartupScreen(),
       navigatorKey: AppNavigator.navigatorKey,
